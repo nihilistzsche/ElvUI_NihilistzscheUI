ed12aa6fb20369b9e0fb7608f4f2c4b8c6d55af0
Nihilistzsche <nihilistzsche@gmail.com>
2023-02-09 00:47:54 -0800

    (Finally) Add vantus runes for Legion, Battle for Azeroth, Shadowlands, and Dragonflight.  Add the Dragonflight augment rune and the inscription runes to the raid prep bar.

--- ElvUI_NihilistzscheUI/modules/utilitybars/bars/raidprepbar.lua
+++ ElvUI_NihilistzscheUI/modules/utilitybars/bars/raidprepbar.lua
@@ -68,6 +68,9 @@ RPB.DataKeys = {
         req = { 40, 50, true },
     },
     {
+        key = "NihilistzscheUI.RaidPrep.LegionVantus",
+    },
+    {
         key = "NihilistzscheUI.RaidPrep.DemonHunter",
     },
     {
@@ -87,6 +90,9 @@ RPB.DataKeys = {
         req = { 45, 50, false, true },
     },
     {
+        key = "NihilistzscheUI.RaidPrep.BattleForAzerothVantus",
+    },
+    {
         key = "NihilistzscheUI.RaidPrep.ShadowlandsFood",
         red = { 50, 60, true, true },
     },
@@ -103,12 +109,25 @@ RPB.DataKeys = {
         req = { 50, 60, false, true },
     },
     {
+        key = "NihilistzscheUI.RaidPrep.ShadowlandsVantus",
+    },
+    {
+        key = "NihilistzscheUI.RaidPrep.DragonflightAugmentRunes",
+        req = { 70, true },
+    },
+    {
         key = "NihilistzscheUI.RaidPrep.DragonflightFood",
         req = { 60, 70, true, true },
     },
     {
         key = "NihilistzscheUI.RaidPrep.DragonflightFlasks",
         req = { 60, 70, false, true },
+    },
+    {
+        key = "NihilistzscheUI.RaidPrep.DragonflightRunes",
+    },
+    {
+        key = "NihilistzscheUI.RaidPrep.DragonflightVantus",
     },
     {
         key = "NihilistzscheUI.RaidPrep.PermanentFlasks",
@@ -177,17 +196,23 @@ function RPB.AddNihilistzscheUIData()
         ["NihilistzscheUI.RaidPrep.LegionAugmentRunes"] = "140587,153023",
         ["NihilistzscheUI.RaidPrep.LegionFlasks"] = "127847,127848,127849,127850,127858",
         ["NihilistzscheUI.RaidPrep.LegionBuffFood"] = "133565,133566,133567,133568,133569,133570,133571,133572,133573,133574,133576,133577,133578,133579,133681",
+        ["NihilistzscheUI.RaidPrep.LegionVantus"] = "128991,128996,128987,128988,128989,128990,128992,128993,128994,128995,129002,142102,142103,146280,146281,146282,146283,146284,146285,146286,146287,146288,148406,151610",
         ["NihilistzscheUI.RaidPrep.DemonHunter"] = "129192,129210",
         ["NihilistzscheUI.RaidPrep.BattleForAzerothAugmentRunes"] = "160053,174906",
         ["NihilistzscheUI.RaidPrep.BattleForAzerothFlasks"] = "152638, 152639, 152640, 152641,168651,168652,168653,168654",
         ["NihilistzscheUI.RaidPrep.BattleForAzerothFood"] = "154887, 154883, 154885, 154882, 154888, 154884, 154886,166344,168314,168313,168311,168310,168312,166804,174352,174350,174349,174348,174351",
-        ["NihilistzscheUI.RaidPrep.ShadowlandsAugmentRunes"] = "181468",
+        ["NihilistzscheUI.RaidPrep.BattleForAzerothVantus"] = "153673,165692,165733,171203",
+        ["NihilistzscheUI.RaidPrep.ShadowlandsAugmentRunes"] = "181468,190384",
         ["NihilistzscheUI.RaidPrep.ShadowlandsFlasks"] = "171276,171278",
         ["NihilistzscheUI.RaidPrep.ShadowlandsOils"] = "171285,171286",
         ["NihilistzscheUI.RaidPrep.ShadowlandsFood"] = "172040,172041,172043,172044,172045,172048,172049,172050,172051,172060,172061,172062,172063,172068,172069",
+        ["NihilistzscheUI.RaidPrep.ShadowlandsVantus"] = "173067,186662,187805",
         ["NihilistzscheUI.RaidPrep.PermanentFlasks"] = "118922,147707",
+        ["NihilistzscheUI.RaidPrep.DragonflightAugmentRunes"] = "201325",
         ["NihilistzscheUI.RaidPrep.DragonflightFood"] = "197772,197774,197775,197776,197777,197778,197779,197780,197781,197782,197783,197784,197785,197786,197787,197788,197789,197790,197791,197792,197793,197794,197795",
         ["NihilistzscheUI.RaidPrep.DragonflightFlasks"] = "191318,191319,191320,191321,191322,191323,191324,191325,191326,191327,191328,191329,191330,191331,191332,191333,191334,191335,191336,191337,191338,191339,191340,191341,191342,191343,191344,191345,191346,191347,191348,191349,191350,191354,191355,191356,191357,191358,191359,195580,197720,197721,197722",
+        ["NihilistzscheUI.RaidPrep.DragonflightRunes"] = "194817,194819,194820,194821,194822,194823,194824,194825,194826",
+        ["NihilistzscheUI.RaidPrep.DragonflightVantus"] = "198491,198492,198493",
         ["NihilistzscheUI.RaidPrep.FishingFood"] = "34832",
     })
 end
