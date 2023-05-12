6b9fb0c0fa377408982da92c79c9e9f639944af8
Nihilistzsche <nihilistzsche@gmail.com>
2023-05-12 02:21:42 -0700

    Add comments to all of the valid trainers and add the new Zaralek Caverns trainers.

--- ElvUI_NihilistzscheUI/modules/petbattleautostart/petbattleautostart.lua
+++ ElvUI_NihilistzscheUI/modules/petbattleautostart/petbattleautostart.lua
@@ -14,71 +14,71 @@ local InPetBattle = _G.C_PetBattles.IsInBattle
 local C_Timer_After = _G.C_Timer.After
 
 PBAS.PetTamers = {
-    [63194] = { valid = true },
-    [64330] = { valid = true },
-    [65648] = { valid = true },
-    [65651] = { valid = true },
-    [65655] = { valid = true },
-    [65656] = { valid = true },
-    [66126] = { valid = true },
-    [66135] = { valid = true },
-    [66136] = { valid = true },
-    [66137] = { valid = true },
-    [66352] = { valid = true },
-    [66372] = { valid = true },
-    [66412] = { valid = true },
-    [66422] = { valid = true },
-    [66436] = { valid = true },
-    [66442] = { valid = true },
-    [66452] = { valid = true },
-    [66466] = { valid = true },
-    [66478] = { valid = true },
-    [66512] = { valid = true },
-    [66515] = { valid = true },
-    [66518] = { valid = true },
-    [66520] = { valid = true },
-    [66522] = { valid = true },
-    [66550] = { valid = true },
-    [66551] = { valid = true },
-    [66552] = { valid = true },
-    [66553] = { valid = true },
-    [66557] = { valid = true },
-    [66635] = { valid = true },
-    [66636] = { valid = true },
-    [66638] = { valid = true },
-    [66639] = { valid = true },
-    [66675] = { valid = true },
-    [66730] = { valid = true },
-    [66733] = { valid = true },
-    [66734] = { valid = true },
-    [66738] = { valid = true },
-    [66739] = { valid = true },
-    [66741] = { valid = true },
-    [66815] = { valid = true },
-    [66819] = { valid = true },
-    [66822] = { valid = true },
-    [66824] = { valid = true },
-    [66918] = { valid = true },
-    [68462] = { valid = true },
-    [68463] = { valid = true },
-    [68464] = { valid = true },
-    [68465] = { valid = true },
-    [71924] = { valid = true },
-    [71926] = { valid = true },
-    [71927] = { valid = true },
-    [71929] = { valid = true },
-    [71930] = { valid = true },
-    [71931] = { valid = true },
-    [71932] = { valid = true },
-    [71933] = { valid = true },
-    [71934] = { valid = true },
-    [73626] = { valid = true },
-    [83837] = { valid = true },
-    [87110] = { valid = true },
-    [87122] = { valid = true },
-    [87123] = { valid = true },
-    [87124] = { valid = true },
-    [87125] = { valid = true },
+    [63194] = { valid = true }, -- Steven Lisbane, Northern Stranglethorn
+    [64330] = { valid = true }, -- Julia Stevens, Elwynn Forest
+    [65648] = { valid = true }, -- Old macDonald, Westfall
+    [65651] = { valid = true }, -- Lindsay, Redright Mountains
+    [65655] = { valid = true }, -- Eric Davidson, Duskwood
+    [65656] = { valid = true }, -- Bill Buckler, The Cape of Stranglethorne
+    [66126] = { valid = true }, -- Zunta, Durotar
+    [66135] = { valid = true }, -- Dagra the Fierce, Northern Barrens
+    [66136] = { valid = true }, -- Analynn, Ashenvale
+    [66137] = { valid = true }, -- Zonya the Sadist, Stonetalon Mountains
+    [66352] = { valid = true }, -- Traitor Gluk, Feralas
+    [66372] = { valid = true }, -- Merda Stronghoof, Desolace
+    [66412] = { valid = true }, -- Elene Flutterfly, Moonglade
+    [66422] = { valid = true }, -- Cassandra Kaboom, Southern Barrens
+    [66436] = { valid = true }, -- Grazzle the Great, Dustwallow March
+    [66442] = { valid = true }, -- Zoltan, Felwood
+    [66452] = { valid = true }, -- Kela Grimtotem, Thousand Needles
+    [66466] = { valid = true }, -- Stone Cold Trixxy, Winterspring
+    [66478] = { valid = true }, -- David Kosse, The Hinterlands
+    [66512] = { valid = true }, -- Deiza Plaguehorn, Eastern Plaguelands
+    [66515] = { valid = true }, -- Koras Darkhammer, Searing Gorge
+    [66518] = { valid = true }, -- Everessa, Swamp of Sorrows
+    [66520] = { valid = true }, -- Durin Darkhammer, Burning Steppes
+    [66522] = { valid = true }, -- Lydia Accoste, Deadwind Pass
+    [66550] = { valid = true }, -- Nicki Tinytech, Hellfire Peninsula
+    [66551] = { valid = true }, -- Ras'an, Zangarmarsh
+    [66552] = { valid = true }, -- Narrok, Nagrand
+    [66553] = { valid = true }, -- Morulu the Elder, Shattrath City
+    [66557] = { valid = true }, -- Bloodknight Antari, Shadowmoon Valley
+    [66635] = { valid = true }, -- Beegle Blastfuse, Howling Fjord
+    [66636] = { valid = true }, -- Nearly Headless Jacob, Crystalsong Forest
+    [66638] = { valid = true }, -- Okrut Dragonwaste, Dragonblight
+    [66639] = { valid = true }, -- Gutretch, Zul'Drak
+    [66675] = { valid = true }, -- Major Payne, Icecrown
+    [66730] = { valid = true }, -- Hyuna of the Shrines, The Jade Forest
+    [66733] = { valid = true }, -- Mo'ruk, Krasarang Wilds
+    [66734] = { valid = true }, -- Farmer Nishi, Valley of the Four Winds
+    [66738] = { valid = true }, -- Corageous Yon, Kun-Lai Summit
+    [66739] = { valid = true }, -- Wastewalker Shu, Dread Wastes
+    [66741] = { valid = true }, -- Aki the Chosen, Vale of Eternal Blossoms
+    [66815] = { valid = true }, -- Bordin Steadyfist, Deepholm
+    [66819] = { valid = true }, -- Brok, Mount Hyjal
+    [66822] = { valid = true }, -- Goz Banefury, Twilight Highlands
+    [66824] = { valid = true }, -- Obalis, Uldum
+    [66918] = { valid = true }, -- Seeker Zushi, Townlong Steppes
+    [68462] = { valid = true }, -- Flowing Pandaren Spiit, Dread Wastes
+    [68463] = { valid = true }, -- Burning Pandaren Spirit, Townlong Steppes
+    [68464] = { valid = true }, -- Whispering Pandaren Spirit, The Jade Forest
+    [68465] = { valid = true }, -- Thundering Pandaren Spirit, Kun-Lai Smmit
+    [71924] = { valid = true }, -- wrathion, Celestial Tournament
+    [71926] = { valid = true }, -- Lorewalker Cho, Celestial Tournament
+    [71927] = { valid = true }, -- Chen Stormstout, Celestial Tournament
+    [71929] = { valid = true }, -- Sully "The Pickle" McLeary Celestial Tournament
+    [71930] = { valid = true }, -- Shademaster Kiryn, Celestial Tournament
+    [71931] = { valid = true }, -- Taran Zhu, Celestial Tournament
+    [71932] = { valid = true }, -- Wise Mari, Celestial Tournament
+    [71933] = { valid = true }, -- Blingtron 4000, Celestial Tournament
+    [71934] = { valid = true }, -- Dr. Ion Goldbloo, Celestial Tournament
+    [73626] = { valid = true }, -- Little Tommy Newcomer, Timeless Isle-
+    [83837] = { valid = true }, -- Cymre Brightblade, Gorgrond
+    [87110] = { valid = true }, -- Tarr the Terrible, Nagrand
+    [87122] = { valid = true }, -- Cargra, Frostfire Ridge
+    [87123] = { valid = true }, -- Vesharr, Spires of Arak
+    [87124] = { valid = true }, -- Ashlei, Shadowmoon Valley
+    [87125] = { valid = true }, -- Taralune, Talador
     [90675] = { valid = true }, -- Erris the Collector (Patch 6.1)
     [91014] = { valid = true }, -- Erris the Collector (Patch 6.1)
     [91015] = { valid = true }, -- Erris the Collector (Patch 6.1)
@@ -91,8 +91,8 @@ PBAS.PetTamers = {
     [91364] = { valid = true }, -- Kura Thunderhoof (Patch 6.1)
     [79858] = { valid = true, noBattle = true }, -- Serr'ah (Patch 6.0)
     [85418] = { valid = true, noBattle = true }, -- Leo the Lioness (Patch 6.0)
-    [67370] = { valid = true, noPopup = true },
-    [85519] = { valid = true, noPopup = true },
+    [67370] = { valid = true, noPopup = true }, -- Jeremy Fease, Darkmoon Island
+    [85519] = { valid = true, noPopup = true }, -- Cristoph VonFeasel,Darkmoon Island
     -- Legion Master Pet Tamers to be verified: some of them can be “petTamersWithoutPopup”
     -- Dalaran (Patch 7.0)
     [107489] = { valid = true }, -- Amalia / Fight Night: Amalia (Family Familiar)
@@ -208,21 +208,26 @@ PBAS.PetTamers = {
     -- Eye of Azshara
     [179694] = { valid = true }, -- Trainer Iris Greedsway
     -- The Waking Shore
-    [189376] = { valid = true, useFirst = true }, -- Swog
+    [189376] = { valid = true }, -- Swog
     [196264] = { valid = true }, -- Haniko
-    [201802] = { valid = true, useFirst = true }, -- Excavtor Morgrum Emberflint
+    [201802] = { valid = true }, -- Excavtor Morgrum Emberflint
     -- The Ohn'ahran Plains
     [197102] = { valid = true }, -- Bakhushek
-    [197447] = { valid = true, useFirst = true }, -- Stormamo
-    [201878] = { valid = true, useFirst = true }, -- Vikshi Thunderpawu
+    [197447] = { valid = true }, -- Stormamo
+    [201878] = { valid = true }, -- Vikshi Thunderpawu
     -- The Azure Span
-    [196069] = { valid = true, useFirst = true }, -- Pachu
-    [197417] = { valid = true, useFirst = true }, -- Arcantus
-    [201899] = { valid = true, useFirst = true }, -- Ixal Whitemoon
+    [196069] = { valid = true }, -- Pachu
+    [197417] = { valid = true }, -- Arcantus
+    [201899] = { valid = true }, -- Ixal Whitemoon
     -- Thaldraszus
-    [197336] = { valid = true, useFirst = true }, -- Enyobon
-    [197350] = { valid = true, useFirst = true }, -- Setimothes
-    [202458] = { valid = true, useFirst = true }, -- Stargazer Xenoth
+    [197336] = { valid = true }, -- Enyobon
+    [197350] = { valid = true }, -- Setimothes
+    [202458] = { valid = true }, -- Stargazer Xenoth
+    -- Zaralek Cavern
+    [201004] = { valid = true }, -- Explorer Bezzert
+    [204792] = { valid = true }, -- Shinmura
+    [204926] = { valid = true }, -- Delver Mardei
+    [204934] = { valid = true }, -- Trainer Orlogg
 }
 
 function PBAS.TAS_OnClick()
