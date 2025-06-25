2f088b676c6abd571e62c1452535c463fab7b27d
Nihilistzsche <nihilistzsche@gmail.com>
2025-06-25 00:48:00 -0700

    Fix rep:standing customdatabar tag as major faction renown is now a format string rather than just a prefix.

--- ElvUI_NihilistzscheUI/modules/customdatabar/tags/reputation.lua
+++ ElvUI_NihilistzscheUI/modules/customdatabar/tags/reputation.lua
@@ -2,12 +2,12 @@ local NUI = _G.unpack((select(2, ...))) --Inport: Engine, Locales, ProfileDB, Gl
 local CDB = NUI.CustomDataBar
 local NT = NUI.Libs.NT
 
-local GetWatchedFactionData = _G.C_Reputation.GetWatchedFactionData
+local C_Reputation_GetWatchedFactionData = _G.C_Reputation.GetWatchedFactionData
 local C_Reputation_IsMajorFaction = _G.C_Reputation.IsMajorFaction
 local C_MajorFactions_GetMajorFactionData = _G.C_MajorFactions.GetMajorFactionData
 function CDB.RegisterRepTags()
     NT:RegisterTag("rep:name", function()
-        local data = GetWatchedFactionData()
+        local data = C_Reputation_GetWatchedFactionData()
 
         if not data then return "" end
 
@@ -15,14 +15,14 @@ function CDB.RegisterRepTags()
     end, "UPDATE_FACTION")
 
     NT:RegisterTag("rep:standing", function()
-        local data = GetWatchedFactionData()
+        local data = C_Reputation_GetWatchedFactionData()
         if not data then return "" end
 
         local isFriend, friendData = NUI.GetFriendshipInfo(data.factionID)
         if NUI.GetParagonInfo(data.factionID) then return "Paragon" end
         if not isFriend and C_Reputation_IsMajorFaction(data.factionID) then
             local majorFactionData = C_MajorFactions_GetMajorFactionData(data.factionID)
-            return RENOWN_LEVEL_LABEL .. majorFactionData.renownLevel
+            return RENOWN_LEVEL_LABEL:format(majorFactionData.renownLevel)
         end
         return isFriend and friendData.reaction or _G["FACTION_STANDING_LABEL" .. data.reaction]
     end, "UPDATE_FACTION")
