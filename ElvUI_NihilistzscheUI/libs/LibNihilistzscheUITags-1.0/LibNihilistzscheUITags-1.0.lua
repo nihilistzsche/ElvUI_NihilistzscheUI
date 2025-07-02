abb0483868c61285b2ded92618956ec464e55567
Nihilistzsche <nihilistzsche@gmail.com>
2025-07-02 02:08:09 -0700

    Do not allow new entries in the tagDirector table.

--- ElvUI_NihilistzscheUI/libs/LibNihilistzscheUITags-1.0/LibNihilistzscheUITags-1.0.lua
+++ ElvUI_NihilistzscheUI/libs/LibNihilistzscheUITags-1.0/LibNihilistzscheUITags-1.0.lua
@@ -1,14 +1,10 @@
 local MAJOR_VERSION = "LibNihilistzscheUITags-1.0"
 local MINOR_VERSION = 1
 
-if not _G.LibStub then
-    error(MAJOR_VERSION .. " requires LibStub")
-end
+if not _G.LibStub then error(MAJOR_VERSION .. " requires LibStub") end
 
 local NihilistzscheLib = _G.LibStub:NewLibrary(MAJOR_VERSION, MINOR_VERSION)
-if not NihilistzscheLib then
-    return
-end
+if not NihilistzscheLib then return end
 
 local strsplit = _G.strsplit
 local CreateFrame = _G.CreateFrame
@@ -20,34 +16,24 @@ NihilistzscheLib.registeredTagStrings = {}
 local mouseoverSeen = {}
 local E
 local function NTags_OnEnter(self)
-    if not E then
-        E = _G.ElvUI[1]
-    end
-    if (mouseoverSeen[self]) then
-        E:UIFrameFadeIn(self, 0.2, 0, 1)
-    end
+    if not E then E = _G.ElvUI[1] end
+    if mouseoverSeen[self] then E:UIFrameFadeIn(self, 0.2, 0, 1) end
 end
 
 local function NTags_OnLeave(self)
-    if not E then
-        E = _G.ElvUI[1]
-    end
-    if (mouseoverSeen[self]) then
-        E:UIFrameFadeOut(self, 0.2, 1, 0)
-    end
+    if not E then E = _G.ElvUI[1] end
+    if mouseoverSeen[self] then E:UIFrameFadeOut(self, 0.2, 1, 0) end
 end
 
 function NihilistzscheLib:RegisterTag(tag, evaluationFunc, events)
-    if not events then
-        events = ""
-    end
+    if not events then events = "" end
 
-    local _events = {strsplit(" ", events)}
+    local _events = { strsplit(" ", events) }
 
-    self.registeredTags[tag] = {evaluationFunc = evaluationFunc, events = _events}
+    self.registeredTags[tag] = { evaluationFunc = evaluationFunc, events = _events }
     for _, event in ipairs(_events) do
-        if (event ~= "") then
-            if (not self.registeredEvents[event]) then
+        if event ~= "" then
+            if not self.registeredEvents[event] then
                 self.driverFrame:RegisterEvent(event)
                 self.registeredEvents[event] = 0
             end
@@ -57,13 +43,11 @@ function NihilistzscheLib:RegisterTag(tag, evaluationFunc, events)
 end
 
 function NihilistzscheLib:UnregisterTag(tag)
-    if (not self.registeredTags[tag]) then
-        return
-    end
+    if not self.registeredTags[tag] then return end
     local events = self.registeredTags[tag].events
     for _, event in ipairs(events) do
         self.registeredEvents[event] = self.registeredEvents[event] - 1
-        if (self.registeredEvents[event] == 0) then
+        if self.registeredEvents[event] == 0 then
             self.driverFrame:UnregisterEvent(event)
             self.registeredEvents[event] = nil
         end
@@ -72,40 +56,25 @@ function NihilistzscheLib:UnregisterTag(tag)
 end
 
 function NihilistzscheLib:RegisterFontString(key, fs)
-    if (not self.registeredTagStrings[key]) then
-        self.registeredTagStrings[key] = {}
-    end
+    if not self.registeredTagStrings[key] then self.registeredTagStrings[key] = {} end
     self.registeredTagStrings[key].fs = fs
     self.registeredTagStrings[key].backingText = ""
     local frame = fs:GetParent()
-    frame:HookScript(
-        "OnEnter",
-        function()
-            if (not NihilistzscheLib.registeredTagStrings[key]) then
-                return
-            end
-            NTags_OnEnter(fs)
-        end
-    )
-    frame:HookScript(
-        "OnLeave",
-        function()
-            if (not NihilistzscheLib.registeredTagStrings[key]) then
-                return
-            end
-            NTags_OnLeave(fs)
-        end
-    )
+    frame:HookScript("OnEnter", function()
+        if not NihilistzscheLib.registeredTagStrings[key] then return end
+        NTags_OnEnter(fs)
+    end)
+    frame:HookScript("OnLeave", function()
+        if not NihilistzscheLib.registeredTagStrings[key] then return end
+        NTags_OnLeave(fs)
+    end)
     -- We own this fs, so only we should change the text
     fs.blzSetText = fs.SetText
-    fs.SetText = function()
-    end
+    fs.SetText = function() end
 end
 
 function NihilistzscheLib:UnregisterFontString(key)
-    if (not self.registeredTagStrings[key]) then
-        return
-    end
+    if not self.registeredTagStrings[key] then return end
     local fs = self.registeredTagStrings[key].fs
     fs.SetText = fs.blzSetText
     fs.blzSetText = nil
@@ -113,52 +82,38 @@ function NihilistzscheLib:UnregisterFontString(key)
 end
 
 function NihilistzscheLib:Tag(key, tagStr)
-    if (not self.registeredTagStrings[key]) then
-        return
-    end
+    if not self.registeredTagStrings[key] then return end
     self.registeredTagStrings[key].backingText = tagStr
     NihilistzscheLib:UpdateTagStrings()
 end
 
 NihilistzscheLib.driverFrame = CreateFrame("Frame")
-NihilistzscheLib.driverFrame:SetScript(
-    "OnEvent",
-    function()
-        NihilistzscheLib:UpdateTagStrings()
-    end
-)
+NihilistzscheLib.driverFrame:SetScript("OnEvent", function() NihilistzscheLib:UpdateTagStrings() end)
 
 local CurrentFS, CurrentKey
 NihilistzscheLib.tagDirector = {}
-setmetatable(
-    NihilistzscheLib.tagDirector,
-    {
-        __index = function(_, key)
-            local customArgs = key:match("{(.-)}%]")
-            if customArgs then
-                key:gsub("{.-}%]", "]")
+setmetatable(NihilistzscheLib.tagDirector, {
+    __index = function(_, key)
+        local customArgs = key:match("{(.-)}%]")
+        if customArgs then key:gsub("{.-}%]", "]") end
+        if NihilistzscheLib.registeredTags[key] then
+            local res = NihilistzscheLib.registeredTags[key].evaluationFunc(CurrentFS, customArgs)
+            if res then
+                NihilistzscheLib.registeredTags[key].cachedResult = res
+                return res
             end
-            if (NihilistzscheLib.registeredTags[key]) then
-                local res = NihilistzscheLib.registeredTags[key].evaluationFunc(CurrentFS, customArgs)
-                if (res) then
-                    NihilistzscheLib.registeredTags[key].cachedResult = res
-                    return res
-                end
-                return NihilistzscheLib.registeredTags[key].cachedResult
-            end
-            return nil
+            return NihilistzscheLib.registeredTags[key].cachedResult
         end
-    }
-)
+        return nil
+    end,
+    __newIndex = function() end,
+})
 
-NihilistzscheLib:RegisterTag(
-    "mouseover",
-    function()
-        local key = CurrentKey
-        mouseoverSeen[key] = true
-        return ""
-    end
-)
+NihilistzscheLib:RegisterTag("mouseover", function()
+    local key = CurrentKey
+    mouseoverSeen[key] = true
+    return ""
+end)
 
 function NihilistzscheLib:UpdateTagStrings()
     for k in pairs(self.registeredTagStrings) do
@@ -172,8 +127,8 @@ function NihilistzscheLib:UpdateTagString(key)
     CurrentFS = _t.fs
     mouseoverSeen[key] = false
     _t.fs:blzSetText(_t.backingText:gsub("%[([^%]]+)%]", self.tagDirector))
-    if (mouseoverSeen[key]) then
-        if (not _t.fs:IsMouseOver()) then
+    if mouseoverSeen[key] then
+        if not _t.fs:IsMouseOver() then
             NTags_OnLeave(_t.fs)
         else
             NTags_OnEnter(_t.fs)
