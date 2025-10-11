330af5b14385286b744510bd07ff41a2d381545e
Nihilistzsche <nihilistzsche@gmail.com>
2025-10-11 02:33:51 -0700

    WoWPro installer updates.

--- ElvUI_NihilistzscheUI_Installer/installers/wowpro.lua
+++ ElvUI_NihilistzscheUI_Installer/installers/wowpro.lua
@@ -2,40 +2,53 @@
 local NUI, E = _G.unpack(_G.ElvUI_NihilistzscheUI)
 local NI = NUI.Installer
 
-NI.WoWProRankCustom = {
-    ["Wyrmrest Accord"] = {
-        Dirishia = 3,
-        Ralaniki = 2,
-        Cerishia = 2,
-        Sayalia = 2,
-        Elaedarel = 2,
-        Alyder = 2,
-        Onaguda = 2,
-        Irgrii = 2,
-        Nilala = 2,
-        Millop = 2,
-        Linabla = 2,
-        Issia = 2,
-        Cherylth = 2,
-    },
-}
+if NUI.NihilPrivate then
+    NI.WoWProRankCustom = {
+        ["Wyrmrest Accord"] = {
+            Uvarha = 3,
+            Dirishia = 3,
+            Ralaniki = 2,
+            Cerishia = 2,
+            Sayalia = 2,
+            Elaedarel = 2,
+            Alyder = 2,
+            Onaguda = 2,
+            Irgrii = 2,
+            Nilala = 2,
+            Millop = 2,
+            Linabla = 2,
+            Issia = 2,
+            Cherylth = 2,
+        },
+    }
 
-function NI:GetWoWProRank()
-    if self.WoWProRankCustom[self.currentRealm] and self.WoWProRankCustom[self.currentRealm][self.currentName] then
-        return self.WoWProRankCustom[self.currentRealm][self.currentName]
+    function NI:GetWoWProRank()
+        if self.WoWProRankCustom[self.currentRealm] and self.WoWProRankCustom[self.currentRealm][self.currentName] then
+            return self.WoWProRankCustom[self.currentRealm][self.currentName]
+        end
+        return 1
     end
-    return 1
 end
 
 function NI:WowProSetup()
+    local LSM = E.Libs.LSM
+
+    local hashtable = LSM:HashTable("font")
+    local fonthash = hashtable[self.db.font]
     local profile = {
-        stepfont = self.db.font,
-        notefont = self.db.font,
-        trackfont = self.db.font,
-        titlefont = self.db.font,
-        stickytitlefont = self.db.font,
+        stepfont = fonthash,
+        steptextsize = 10,
+        notefont = fonthash,
+        notetextsize = 8,
+        trackfont = fonthash,
+        tracktextsize = 8,
+        titlefont = fonthash,
+        titletextsize = 10,
+        stickytitlefont = fonthash,
+        sticktitletextsize = 10,
+        position = { "TOP", "UIParent", "TOP", 418, -48 },
     }
-    if NUI.Private then profile.rank = self:GetWoWProRank() end
+    if NUI.NihilPrivate then profile.rank = self:GetWoWProRank() end
     self:SetProfile(_G.WoWProData, profile, self.baseProfile)
 end
 
